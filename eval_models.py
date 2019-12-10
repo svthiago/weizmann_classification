7de61dbe08ef80688d9f244d32de769443dabc17
C1299700 Thiago Sales Vilela <c1299700@bb.com.br>
2019-12-10 12:53:21 -0300

    Eval models bug fix

--- eval_models.py
+++ eval_models.py
@@ -7,35 +7,34 @@ def load_by_person(load_name, img_width, img_heigth):
     
     folders = sorted(glob(path), key=str)
 
+    # load a new frame
+    new_frame = cv2.imread(path + folder + "/" + frame)
 
-        # load a new frame
-        new_frame = cv2.imread(path + folder + "/" + frame)
+    # convert to grayscale
+    new_frame = cv2.cvtColor(new_frame, cv2.COLOR_RGB2GRAY)
+    # resize frame
+    new_frame = cv2.resize(new_frame, (img_width, img_heigth))
 
-        # convert to grayscale
-        new_frame = cv2.cvtColor(new_frame, cv2.COLOR_RGB2GRAY)
-        # resize frame
-        new_frame = cv2.resize(new_frame, (img_width, img_heigth))
+    # X.append(new_frame.astype("float32")/255.)
+    # Y.append(folder)
 
-        # X.append(new_frame.astype("float32")/255.)
-        # Y.append(folder)
+    name = frame.split('_')[0]
 
-        name = frame.split('_')[0]
+    if re.match(train_names, name):
+        train_data.append(new_frame.astype("float32")/255.)
+        train_labels.append(folder)
 
-        if re.match(train_names, name):
-            train_data.append(new_frame.astype("float32")/255.)
-            train_labels.append(folder)
+        flipped_frame = cv2.flip(new_frame.astype("float32")/255., 1)
+        train_data.append(flipped_frame)
+        train_labels.append(folder)
 
-            flipped_frame = cv2.flip(new_frame.astype("float32")/255., 1)
-            train_data.append(flipped_frame)
-            train_labels.append(folder)
+    elif re.match(eval_name, name):
+        eval_data.append(new_frame.astype("float32")/255.)
+        eval_labels.append(folder)
 
-        elif re.match(eval_name, name):
-            eval_data.append(new_frame.astype("float32")/255.)
-            eval_labels.append(folder)
-
-            flipped_frame = cv2.flip(new_frame.astype("float32")/255., 1)
-            train_data.append(flipped_frame)
-            train_labels.append(folder)
+        flipped_frame = cv2.flip(new_frame.astype("float32")/255., 1)
+        train_data.append(flipped_frame)
+        train_labels.append(folder)
 
 
     # X = np.array(X).reshape((-1, img_width, img_heigth, 1))
